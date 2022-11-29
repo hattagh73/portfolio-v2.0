2b456174815db4a6f23e230ecb96f60b564f5da9
hattagh73 <>
2022-11-29 20:52:20 +0800

    add-app

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+//* NextJS Features
+import type { AppProps } from 'next/app';
+
+//* Layout Component
+import Layout from '../components/Layout';
+
+//* Global Styling
+import '../styles/globals.css';
+
+const App = ({ Component, pageProps}: AppProps) => {
+    return (
+        <>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    )
+}
+export default App;
