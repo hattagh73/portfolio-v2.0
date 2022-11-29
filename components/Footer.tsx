0195d307818a672d42a758b482d8175f6819b263
hattagh73 <>
2022-11-29 20:52:59 +0800

    add-component-footer

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="pb-16 md:pb-0">
+            
+            <div className="max-w-screen-2xl px-6 md:mx-auto">
+                <p>This is a footer</p>
+            </div>
+            
+        </footer>
+    )
+}
+export default Footer;
