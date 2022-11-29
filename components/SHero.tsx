ce1a65b5a32ba1aef49df611468877aff769fd85
hattagh73 <>
2022-11-30 03:40:35 +0800

    update-hero

--- components/SHero.tsx
+++ components/SHero.tsx
@@ -1,10 +1,10 @@
 const SHero = () => {
     return (
-        <section className="min-h-[50vh] border md:pt-10">
+        <section className="min-h-[70vh] border md:pt-20 bg-lime-50">
             
             <div className="max-w-screen-2xl px-6 md:mx-auto">
-                <h1 className="text-5xl font-secondary font-bold">Welcome To My Portfolio Site</h1>
-                <p className="text-lg">Quiche Font</p>
+                <h1 className="text-6xl font-secondary font-extrabold">Welcome To My Portfolio Site</h1>
+                <p className="text-lg font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi suscipit eum voluptatum excepturi illo facilis cumque nisi expedita, dolor beatae hic in a nemo architecto consequuntur modi provident at veniam.</p>
             </div>
             
         </section>
