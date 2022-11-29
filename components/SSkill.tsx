8da734885f4cfbd544e48793360f2d904dcbc595
hattagh73 <>
2022-11-29 21:19:06 +0800

    add-component-skill

---
+++ components/SSkill.tsx
@@ -0,0 +1,12 @@
+const SSkill = () => {
+    return (
+        <section className="h-screen">
+            
+            <div className="max-w-screen-2xl px-6 md:mx-auto">
+                <p>This is a Skill Section</p>
+            </div>
+            
+        </section>
+    )
+}
+export default SSkill;
