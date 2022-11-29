5b0977f84560fee55c2a6c4ae806b6dc1426ba35
hattagh73 <>
2022-11-29 21:18:50 +0800

    add-component-about

---
+++ components/SAbout.tsx
@@ -0,0 +1,12 @@
+const SAbout = () => {
+    return (
+        <section className="h-screen">
+            
+            <div className="max-w-screen-2xl px-6 md:mx-auto">
+                <p>This is a About Section</p>
+            </div>
+            
+        </section>
+    )
+}
+export default SAbout;
