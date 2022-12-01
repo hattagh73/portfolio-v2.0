a097b48ec166c60483b31d200c24e54e77545d24
hattagh73 <>
2022-12-01 19:19:04 +0800

    update-component-hero

--- components/SHero.tsx
+++ components/SHero.tsx
@@ -1,12 +1,68 @@
+import Link from 'next/link';
+import classNames from 'classnames';
+
 const SHero = () => {
     return (
-        <section className="min-h-[70vh] border md:pt-20 bg-lime-50">
+        <section 
+            id="home" 
+            className={classNames(
+                "bg-lime-100",
+                // "pt-4",
+                "md:pt-20 md:bg-transparent",
+            )}
+        >
             
-            <div className="max-w-screen-2xl px-6 md:mx-auto">
-                <h1 className="text-6xl font-secondary font-extrabold">Welcome To My Portfolio Site</h1>
-                <p className="text-lg font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi suscipit eum voluptatum excepturi illo facilis cumque nisi expedita, dolor beatae hic in a nemo architecto consequuntur modi provident at veniam.</p>
-            </div>
-            
+            {/* START Container */}
+            <div
+                className={classNames(
+                    "max-w-screen-2xl mx-6 2xl:mx-auto" //! Container
+                )}
+            >
+                {/* START Hero Card */}
+                <div
+                    className={classNames(
+                        "rounded-lg flex flex-col",
+                        "py-6", //! Mobile
+                        "md:bg-lime-100 md:px-6 md:py-28 text-[#262626] md:place-items-center",
+                    )}
+                >
+                    {/* Hi */}
+                    {/* <p className="text-[#666666]">hi there! 👋</p> */}
+                    <p className="">hi there! 👋</p>
+                    
+                    {/* Welcome */}
+                    <h1 
+                        className="text-5xl md:text-center md:text-6xl font-extrabold font-quiche max-w-screen-sm"
+                    >Welcome to my <span className="text-red-600">Personal <span className="">Portfolio</span></span>
+                    </h1>
+                    
+                    
+                    {/* Quote Explore */}
+                    <div className="my-14">
+                        <p 
+                            className="text-2xl md:text-center"
+                            // contentEditable="true"
+                        >Exploring the digital world on multi-platform mainly in design
+                        </p>
+                    </div>
+                    
+                    {/* Learn more */}
+                    <div>
+                        <Link 
+                            className={classNames(
+                                "px-10 py-3 rounded-full cursor-pointer inline-flex",
+                                "border border-black/10",
+                                "shadow-transparent transition duration-300 ease-linear",
+                                "hover:shadow-lg"
+                            )}
+                            href="#about" 
+                            scroll={false}
+                        >Learn More
+                        </Link>
+                    </div>
+
+                </div>{/* END Hero Card */}
+            </div>{/* END Container */}
         </section>
     )
 }
