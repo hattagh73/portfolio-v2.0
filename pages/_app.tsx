d7ca672f26c31be10a9690019208b1e5f058e712
hattagh73 <>
2022-12-01 19:25:01 +0800

    update-app-use-variety-fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,55 +8,104 @@ import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 //* Font Online & Local
-import { Poppins } from '@next/font/google';
+import { Inter } from '@next/font/google';
 import localFont from '@next/font/local';
 
-const poppins = Poppins({
-    weight: ['300', '400', '500', '600'],
-    style: ['normal', 'italic'],
+//* Font Inter
+const inter = Inter({
     subsets: ['latin'],
 });
 
+//* Font Local Quiche
 const quiche = localFont({ 
     src: [
         {
-            path: '../public/fonts/Fontspring-DEMO-quichesans-bold.otf',
+            path: '../public/fonts/Quichesans-Bold.otf',
             weight: '700'
         },
         {
-            path: '../public/fonts/Fontspring-DEMO-quichesans-extrabold.otf',
+            path: '../public/fonts/Quichesans-Extrabold.otf',
             weight: '800'
         }
     ],
-    variable: '--font-quiche'
+    variable: '--font-quiche',
+    display: 'swap',
+    preload: false
 });
 
-const grandis = localFont({ 
-    src: [
+//* Font Local ClashDisplay
+const clashDisplay = localFont({
+    src:[
         {
-            path: '../public/fonts/GrandisExtended-Bold.woff2',
-            weight: '700'
+            path: '../public/fonts/ClashDisplay-Medium.otf',
+            weight: '500',
+            style: 'normal'
+        },
+        {
+            path: '../public/fonts/ClashDisplay-Semibold.otf',
+            weight: '600',
+            style: 'normal'
+        },
+        {
+            path: '../public/fonts/ClashDisplay-Bold.otf',
+            weight: '700',
+            style: 'normal'
         },
     ],
-    variable: '--font-grandis'
-});
+    variable: '--font-clashDisplay',
+    display: 'swap',
+    preload: false
+})
+
+//* Font Local Satoshi
+const satoshi = localFont({
+    src:[
+        {
+            path: '../public/fonts/Satoshi-Light.otf',
+            weight: '300',
+            style: 'normal'
+        },
+        {
+            path: '../public/fonts/Satoshi-Regular.otf',
+            weight: '400',
+            style: 'normal'
+        },
+        {
+            path: '../public/fonts/Satoshi-Medium.otf',
+            weight: '500',
+            style: 'normal'
+        }
+    ],
+    variable: '--font-satoshi',
+    display: 'swap',
+    preload: false
+})
+
 
 const App = ({ Component, pageProps}: AppProps) => {
     // console.log('the fonts', quiche)
     return (
         <>
+
+            {/* Declare Fonts To Used */}
             <style jsx global>{`
                 :root {
                     --quiche-font: ${quiche.style.fontFamily};
-                    --poppins-font: ${poppins.style.fontFamily};
-                    --grandis-font: ${grandis.style.fontFamily};
+                    --inter-font: ${inter.style.fontFamily};
+                    --clashDisplay-font: ${clashDisplay.style.fontFamily};
+                    --satoshi-font: ${satoshi.style.fontFamily};
+                }
+                html {
+                    scroll-behavior: smooth;
+                    // font-family: ${inter.style.fontFamily}
+                    font-family: ${satoshi.style.fontFamily}
                 }
             `}</style>
-            <main className={`font-primary relative z-[1]`}>
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
-            </main>
+            
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+
         </>
     )
 }
