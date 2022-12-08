8a18cb72870b3ba7f1901d479c434ae64a11d45d
hattagh73 <>
2022-12-08 17:26:18 +0800

    update-app-add-icon-inside-head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 //* NextJS Features
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 //* Layout Component
@@ -11,7 +12,16 @@ const App = ({ Component, pageProps}: AppProps) => {
     
     return (
         <>
+            {/* Global Head */}
+            <Head>
+                <title>hattaltd.</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/fav-icon/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/fav-icon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/fav-icon/favicon-16x16.png" />
+                <link rel="manifest" href="/fav-icon/site.webmanifest"></link>
+            </Head>
 
+            {/* Header - Page - Footer */}
             <Layout>
                 <Component {...pageProps} />
             </Layout>
