15eabdd343db45342f51c1e33a9cd243be9ded9f
hattagh73 <>
2022-12-08 17:24:51 +0800

    update-components

--- components/components.ts
+++ components/components.ts
@@ -3,10 +3,10 @@ export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 
 //* Import Section Components
-export { default as  SHero} from './SHero';
-export { default as  SAbout} from './SAbout';
-export { default as  SSkill} from './SSkill';
-export { default as  SProject} from './SProject';
+export { default as  SHero} from './sections/SHero';
+export { default as  SAbout} from './sections/SAbout';
+export { default as  SSkill} from './sections/SSkill';
+export { default as  SProject} from './sections/SProject';
 
 //* Import Icon/ Img Components
 export { default as FiFigma } from './icons/FiFigma';
@@ -15,3 +15,4 @@ export { default as FiGitHub } from './icons/FiGitHub';
 //* Import Reusable Component
 export { default as  Logo} from './Logo';
 export { default as  SocialListIcon} from './SocialListIcon';
+export { default as  Blob} from './Blob';
